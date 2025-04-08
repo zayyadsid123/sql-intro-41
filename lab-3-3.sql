85c33b734b9d4b7559e07368e0e1a2d94f02b797
Zayyad Siddiqui <zayyad.siddiqui@kellogg.northwestern.edu>
2025-04-08 16:30:27 +0000

    completed lab 3 continued and 4

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -1,7 +1,7 @@
 -- In the modern era (1960-present), how many regular season games did the best team win each season?
 -- Challenge: Try to include the team's name in the results. Is it accurate?
 
-select year, name, MAX(wins) from teams 
+select year, MAX(wins) from teams 
 WHERE year>=1960 
 GROUP BY year 
 ORDER BY MAX(wins) DESC;
--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,8 @@
 -- How many lifetime hits does Barry Bonds have?
-
+select players.first_name,players.last_name,players.id, SUM(stats.hits) from players 
+INNER JOIN stats ON players.id=stats.player_id 
+WHERE players.first_name="Barry" AND players.last_name="Bonds" 
+GROUP BY players.id
 -- Expected result:
 -- 2935
 
