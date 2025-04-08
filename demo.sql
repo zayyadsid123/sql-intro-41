6fc39ea4b0df7eeef026edf8e8fecd17c6390962
Zayyad Siddiqui <zayyad.siddiqui@kellogg.northwestern.edu>
2025-04-08 14:38:41 +0000

    finding losingest seasons in lab-1-3.sql

--- demo.sql
+++ demo.sql
@@ -1,4 +1,6 @@
 -- Get the first and last name of every player in the database.
 -- Note: this is a code comment in SQL (meaning it's documentation and ignored by the computer).
 
+-- My comment
+
 SELECT first_name, last_name FROM players;
--- lab-1-3.sql
+++ lab-1-3.sql
@@ -1,5 +1,7 @@
 -- Which three teams had the losingest seasons?
 
+SELECT year, name, wins, losses FROM teams ORDER BY losses DESC LIMIT 3;
+
 -- Expected result:
 --
 -- +------+----------------------+------+--------+
